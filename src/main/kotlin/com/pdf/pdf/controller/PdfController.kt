30729cbf0b8fab9e11a5d4f8c344d73648849dfe
Daniel-Flores-S <danielfloresda802@gmail.com>
2023-03-03 17:33:44 -0300

    .

--- src/main/kotlin/com/pdf/pdf/controller/PdfController.kt
+++ src/main/kotlin/com/pdf/pdf/controller/PdfController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile
 import java.io.ByteArrayOutputStream
 
 @RestController
-@CrossOrigin(origins = ["*"], allowedHeaders = ["*"])
+@CrossOrigin("*")
 class ConvertController {
     @PostMapping("/convert")
     fun convertPdfToExcel(@RequestParam("file") file: MultipartFile): ResponseEntity<ByteArray> {
