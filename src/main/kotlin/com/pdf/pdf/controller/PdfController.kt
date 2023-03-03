52ed9e5f9d0271c53cfbd974096ff85655bf8284
Daniel-Flores-S <danielfloresda802@gmail.com>
2023-03-03 17:17:56 -0300

    .

--- src/main/kotlin/com/pdf/pdf/controller/PdfController.kt
+++ src/main/kotlin/com/pdf/pdf/controller/PdfController.kt
@@ -6,6 +6,7 @@ import org.apache.pdfbox.text.PDFTextStripper
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile
 import java.io.ByteArrayOutputStream
 
 @RestController
+@CrossOrigin(origins = ["*"], allowedHeaders = ["*"])
 class ConvertController {
     @PostMapping("/convert")
     fun convertPdfToExcel(@RequestParam("file") file: MultipartFile): ResponseEntity<ByteArray> {
